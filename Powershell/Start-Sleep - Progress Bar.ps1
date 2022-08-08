e66d88cc12bf5b666ac48ae45f8bab2afd343576
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-08-08 15:28:50 -0400

    Create Start-Sleep - Progress Bar.ps1

---
+++ Powershell/Start-Sleep - Progress Bar.ps1
@@ -0,0 +1,28 @@
+#region Time Left (Seconds)
+# Set 
+[int]$Time = 10
+$Lenght = $Time / 100
+For ($Time; $Time -gt 0; $Time--) {
+$min = [int](([string]($Time/60)).split('.')[0])
+$text = " " + $min + " minutes " + ($Time % 60) + " seconds left"
+Write-Progress -Activity "Watiting for..." -Status $Text -PercentComplete ($Time / $Lenght)
+Start-Sleep 1
+}
+#endregion
+
+$i = 30
+
+do {
+    Write-Host $i
+    Sleep 1
+    $i--
+} while ($i -gt 0)
+
+$Seconds = 30
+$EndTime = [datetime]::UtcNow.AddSeconds($Seconds)
+
+while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
+  Write-Progress -Activity 'Watiting for...' -Status Godot -SecondsRemaining $TimeRemaining.TotalSeconds
+  Start-Sleep 0
+  cls
+}
