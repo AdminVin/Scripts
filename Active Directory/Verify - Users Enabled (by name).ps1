12dc9e83895ac81f6aacde0aeaaa98d1609e6eb5
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-08-12 08:06:23 -0400

    Update 2022-08-12

---
+++ Active Directory/Verify - Users Enabled (by name).ps1
@@ -0,0 +1,22 @@
+#region Import Modules
+Import-Module ActiveDirectory
+
+#endregion
+
+
+#region Process Verification
+# Import CSV
+$CSV = Import-CSV "Verify - Users Enabled (by name).csv"
+
+# Loop through accounts in CSV and verify all accounts
+foreach ($Users in $CSV)
+{
+	# Read user data from each field in each row and assign the data to a variable as below
+	$Username = $Users.FirstName + "." + $Users.Lastname	
+	# Full Command Example
+	# Get-ADUser $Username -Properties ProxyAddresses,Mail,ExtensionAttribute1 | Select-Object Mail,ExtensionAttribute1,ProxyAddresses, @{Name='PrimarySMTPAddress';Expression={$_.ProxyAddresses -cmatch '^SMTP:' -creplace 'SMTP:'}}
+	# Truncated Command
+	Get-ADUser $Username -Properties UserPrincipalName,employeeNumber,ProxyAddresses | Select-Object Enabled,Samaccountname,employeeNumber,@{Name='PrimarySMTPAddress';Expression={$_.ProxyAddresses -cmatch '^SMTP:' -creplace 'SMTP:'}} 
+}
+
+#endregion
