9d2fa3eaad835626f385db21d4c038ecccbfa120
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-08-12 15:24:08 -0400

    Update 2022-08-12

--- Active Directory/Rename - User.ps1
+++ Active Directory/Rename - User.ps1
@@ -5,10 +5,10 @@ Import-Module ActiveDirectory
 
 #region Varibles / Settings
 Set-PSDebug -Off
-$Domain = "@DOMAIN.com"
+$Domain = "@DOMAIN.org"
 $MicrosoftDomain = "@DOMAIN.onmicrosoft.com"
-$UsernameOLD = Read-Host "Please enter in the EXISTING username for the user you wish to rename"
-CLS
+Write-Host "Please enter in the EXISTING username for the user you wish to rename" -ForegroundColor Yellow
+$UsernameOLD = Read-Host "EXISTING Username"
 
 #endregion
 
@@ -17,7 +17,7 @@ CLS
 Write-Host "*****************************" -ForegroundColor Red
 Write-Host "* EXISTING USER INFORMATION *" -ForegroundColor Red
 Write-Host "*****************************" -ForegroundColor Red
-
+Write-Host " "
 
 Get-ADUser $UsernameOLD -Properties UserPrincipalName,employeeNumber,ProxyAddresses | Select-Object Enabled,Samaccountname,GivenName,Surname,employeeNumber,@{Name='PrimarySMTPAddress';Expression={$_.ProxyAddresses -cmatch '^SMTP:' -creplace 'SMTP:'}} 
 $EmailOLD = Get-ADUser $UsernameOLD -Properties ProxyAddresses | Select-Object @{Name='PrimarySMTPAddress';Expression={$_.ProxyAddresses -cmatch '^SMTP:' -creplace 'SMTP:'}} 
@@ -37,8 +37,7 @@ $LastNameNEW = Read-Host "Please enter in the NEW last name"
 $UsernameNEW = $FirstNameNEW + "." + $LastNameNEW
 $EmailNEW = $UsernameNEW+$Domain
 Write-Host " "
-Write-Host "New Account Name:"$UsernameNEW -ForegroundColor Yellow
-Write-Host "New Email Address:"$EmailNEW -ForegroundColor Yellow
+
 
 #endregion
 
@@ -51,12 +50,16 @@ Set-ADUser $UsernameOLD -Replace @{samaccountname=$UsernameNEW}
 # Update Name [General Tab]
 Set-ADUser $UsernameNEW -GivenName $FirstNameNEW -Surname $LastNameNEW -DisplayName $FirstNameNEW" "$LastNameNEW
 Get-Aduser $UsernameNEW | Rename-ADObject -NewName "$FirstNameNEW $LastNameNEW"
+Write-Host "New Account Name:"$UsernameNEW -ForegroundColor Yellow
 
 # User Login [Local Account on Local System] / Not relevant for AD Enviroment
 # Rename-LocalUser -Name $UsernameOLD -NewName $UsernameNEW
 
 # Email [Attribute Editor Tab > mail]
 Set-ADUser -Identity $UsernameNEW -Email $EmailNEW
+Set-ADUser -Identity $UsernameNEW -Clear mailNickname
+Set-ADUser -Identity $UsernameNEW -Add @{mailNickname=$UsernameNEW}
+Write-Host "New Email Address:"$EmailNEW -ForegroundColor Yellow
 
 # Proxy Addresses [Attribute Editor Tab > Proxy Addresses]
 $ProxyAddressOLD = $UsernameOLD+$MicrosoftDomain
@@ -70,4 +73,27 @@ Set-ADUser -Identity $UsernameNEW -Add @{ProxyAddresses="smtp:"+$ProxyAddressOLD
 Set-ADUser -Identity $UsernameNEW -Add @{ProxyAddresses="SMTP:"+$ProxyAddressNEW}
 Set-ADUser -Identity $UsernameNEW -Add @{ProxyAddresses="smtp:"+$ProxyAddressNEW2}
 
+# Email [Attribute Editor Tab > Target Address]
+Set-ADUser -Identity $UsernameNEW -Clear targetAddress
+Set-ADUser -Identity $UsernameNEW -Add @{targetAddress=$UsernameNEW+$MicrosoftDomain}
+Write-Host "New Target Email Address:"$UsernameNEW$MicrosoftDomain -ForegroundColor Yellow
+
+# msExchArchiveName [Attribute Editor Tab > mxExchArchiveName]
+$msExchArchiveNameNEW = "In-Place Archive -"+" "+$FirstNameNEW+" "+$LastNameNEW
+Set-ADUser -Identity $UsernameNEW -Clear msExchArchiveName
+Set-ADUser -Identity $UsernameNEW -Add @{msExchArchiveName=$msExchArchiveNameNEW}
+Write-Host "New mxExchArchiveName: "$msExchArchiveNameNew -ForegroundColor Yellow
+
+# Home Directory [Attribute Editor Tab > homeDirectory]
+$NewHomeDir = "C:\users\"+$UsernameNEW
+Set-ADUser $UsernameNew -HomeDirectory $NewHomeDir
+Write-Host "New homeDirectory:"$NewHomeDir -ForegroundColor Yellow
+
+# Description
+$Date = Get-Date -UFormat "%Y-%m-%d %R"
+Set-ADUser $UsernameNEW -Description "Updated: $Date"
+
 #endregion
+
+Write-Host " " -ForegroundColor Yellow
+Write-Host "If the above looks correct, run a DirSync (Delta Update) to sync the changes Azure/Office 365" -ForegroundColor Yellow
