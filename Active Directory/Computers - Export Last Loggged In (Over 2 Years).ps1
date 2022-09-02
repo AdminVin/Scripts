2cf5a6f0a29bc6a5fd000fab15070e4c49bf26cd
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-09-02 09:43:00 -0400

    Update 2022/09/02

---
+++ Active Directory/Computers - Export Last Loggged In (Over 2 Years).ps1
@@ -0,0 +1,18 @@
+#region Varibles
+# Set location of OU to search
+$SearchOU = ‘DC=DOMAIN,DC=local’
+# Set the number of days to exclude
+$LastLogonDate= (Get-Date).AddDays(-730)
+#endregion
+
+# Search & View ONLY Computer Objects
+Get-ADComputer -Properties LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Sort-Object LastLogonTimeStamp | Format-Table Name, @{N='lastlogontimestamp'; E={[DateTime]::FromFileTime($_.lastlogontimestamp)}} -AutoSize
+
+# Search & Export Computer Objects List
+Get-ADComputer -Properties Enabled,DNSHostName,LastLogonDate,LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Select-Object Enabled,DNSHostName,LastLogonDate,DistinguishedName | Export-CSV "Computers-ExportLastLoggedIn(Over2Years).csv"
+
+# Search & Disable Computer Objects
+#Get-ADComputer -Properties LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Sort-Object LastLogonTimeStamp| Format-Table Name, @{N='lastlogontimestamp'; E={[DateTime]::FromFileTime($_.lastlogontimestamp)}} -AutoSize | Disable-ADAccount
+
+# Search & Delete Computer Objects
+#Get-ADComputer -Properties LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Sort-Object LastLogonTimeStamp| Format-Table Name, @{N='lastlogontimestamp'; E={[DateTime]::FromFileTime($_.lastlogontimestamp)}} -AutoSize | Remove-ADComputer
