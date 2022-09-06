17a47826e0ed6b7d0fef06844ab58372320b5669
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-09-06 08:28:57 -0400

    Update 2022-09-06

--- Active Directory/Security Groups - Export Group Members.ps1
+++ Active Directory/Security Groups - Export Group Members.ps1
@@ -1,2 +1,2 @@
 # Export all members from an Active Directory Group to CSV
-Get-ADGroupMember -identity "AlreadyProofedMailboxSEC" | select name | Export-CSV Alreadyproofed.csv -Notypeinformation
+Get-ADGroupMember -identity "SecurityGroupName" | select name | Export-CSV "SecurityGroup-ExportGroupMembers.csv" -Notypeinformation
