2582c180a7488929924296c0d9470f6ea2411902
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-09-07 16:04:57 -0400

    Update 2022-09-07

--- Active Directory/Computers - Export Last Loggged In (Over 2 Years).ps1
+++ Active Directory/Computers - Export Last Loggged In (Over 2 Years).ps1
@@ -1,6 +1,6 @@
 #region Varibles
 # Set location of OU to search
-$SearchOU = ‘DC=DOMAIN,DC=local’
+$SearchOU = "DC=DOMAIN,DC=LOCAL"
 # Set the number of days to exclude
 $LastLogonDate= (Get-Date).AddDays(-730)
 #endregion
@@ -9,7 +9,19 @@ $LastLogonDate= (Get-Date).AddDays(-730)
 Get-ADComputer -Properties LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Sort-Object LastLogonTimeStamp | Format-Table Name, @{N='lastlogontimestamp'; E={[DateTime]::FromFileTime($_.lastlogontimestamp)}} -AutoSize
 
 # Search & Export Computer Objects List
-Get-ADComputer -Properties Enabled,DNSHostName,LastLogonDate,LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Select-Object Enabled,DNSHostName,LastLogonDate,DistinguishedName | Export-CSV "Computers-ExportLastLoggedIn(Over2Years).csv"
+Get-ADComputer -Properties Enabled,Name,LastLogonDate,LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Select-Object Enabled,Name,LastLogonDate,DistinguishedName | Export-CSV "Computers-ExportLastLoggedIn(Over2Years).csv"
+
+# Disable and Move Computers to Specified OU from CSV Export
+$DestinationOU = "OU=OU_NAME,OU=OU_NAME,OU=OU_NAME,OU=OU_NAME,DC=DOMAIN,DC=LOCAL"
+$CSV = Import-CSV "Computers-ExportLastLoggedIn(Over2Years).csv"
+foreach ($PC in $CSV)
+{
+	$PCName	= $PC.Name
+    $PCDescription = Get-ADComputer $PCName | Select-Object DistinguishedName
+    Get-ADComputer $PCName | Set-ADComputer -Description "Original OU: $PCDescription"
+	Get-ADComputer $PCName | Disable-ADAccount -PassThru | Move-ADObject -TargetPath $DestinationOU
+	Write-Output "$PCName disabled, and moved to $DestinationOU."
+    }
 
 # Search & Disable Computer Objects
 #Get-ADComputer -Properties LastLogonTimeStamp -Filter {LastLogonTimeStamp -lt $LastLogonDate } -SearchBase $SearchOU | Sort-Object LastLogonTimeStamp| Format-Table Name, @{N='lastlogontimestamp'; E={[DateTime]::FromFileTime($_.lastlogontimestamp)}} -AutoSize | Disable-ADAccount
--- Active Directory/Security Groups - Export Group Members.ps1
+++ Active Directory/Security Groups - Export Group Members.ps1
@@ -1,2 +1,2 @@
 # Export all members from an Active Directory Group to CSV
-Get-ADGroupMember -identity "SecurityGroupName" | select name | Export-CSV "SecurityGroup-ExportGroupMembers.csv" -Notypeinformation
+Get-ADGroupMember -identity "SecurityGroupName" | Select-Object Name | Export-CSV "SecurityGroup-ExportGroupMembers.csv" -Notypeinformation
