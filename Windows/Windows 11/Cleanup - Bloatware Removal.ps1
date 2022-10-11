df0f04b08a28cf7e319dbd25c5c7741ebef45d25
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-11 17:53:16 -0400

    Update 2022-10-11

--- Windows/Windows 11/Cleanup - Bloatware Removal.ps1
+++ Windows/Windows 11/Cleanup - Bloatware Removal.ps1
@@ -160,7 +160,7 @@ Set-Location C:\
 Write-Host "3.2.2 Disabled Cortana Web Search" -ForegroundColor YELLOW
 
 # 3.3 Widgets
-winget uninstall "Windows web experience pack"
+winget uninstall "Windows web experience pack" --force
 Write-Host "3.3 Widgets Removal" -ForegroundColor YELLOW
 
 #endregion
