2656770d9843e0ddecd726442ca6443ec8cdc5b6
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-14 13:23:46 -0400

    Update 2022-10-14

--- Active Directory/Replication - Remote Sync Domain Controllers.ps1
+++ Active Directory/Replication - Remote Sync Domain Controllers.ps1
@@ -1,6 +1,15 @@
 #region Notes
-# Start > Settings > Apps > Optional Features
-# Install - RSAT: Active Directory Domain Services and Light Weight Directory Service Tools
+<#
+Start > Settings > Apps > Optional Features
+Install - RSAT: Active Directory Domain Services and Light Weight Directory Service Tools
+
+Flags Definition
+/syncall: Synchronizes a specified domain controller with all replication partners.
+/A: Perform /SyncAll for all NC’s held by <Dest DSA> (ignores <Naming Context>)
+/d: ID servers by DN in messages (instead of GUID DNS)
+/e: Enterprise, cross sites (default: only home site)
+/P: Push changes outward from home server (default: pull changes)
+#>
 #endregion
 
 #region Process Sync - Local
