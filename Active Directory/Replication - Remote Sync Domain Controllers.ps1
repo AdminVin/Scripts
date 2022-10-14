466d0fdf8034d9b9064e8a0635e0af3a26495456
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-14 13:11:46 -0400

    Update 2022-10-14

--- Active Directory/Replication - Remote Sync Domain Controllers.ps1
+++ Active Directory/Replication - Remote Sync Domain Controllers.ps1
@@ -3,6 +3,10 @@
 # Install - RSAT: Active Directory Domain Services and Light Weight Directory Service Tools
 #endregion
 
-#region Process Sync
+#region Process Sync - Local
+repadmin /syncall /AdeP
+#endregion
+
+#region Process Sync - Remote
 Invoke-Command -ComputerName SERVERNAME -ScriptBlock {repadmin /syncall /AdeP}
 #endregion
