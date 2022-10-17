3fc8c36f855f30f9b0c405e9e3313e36e0dd0b76
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-17 18:19:37 -0400

    Update 2022-10-17

--- Printers/Queue - Management and Error Notification.ps1
+++ Printers/Queue - Management and Error Notification.ps1
@@ -9,12 +9,6 @@ $ComputerNames = @("SERVER")
 Invoke-Command -ComputerName EBPRINT1 -ScriptBlock {Get-WMIObject Win32_PerfFormattedData_Spooler_PrintQueue | Where-Object {($_.Name -ne "PaperCut") -AND ($_.JobErrors -gt "0")} | Select-Object Name, @{Expression={$_.jobs};Label="CurrentJobs"}, JobErrors, TotalJobsPrinted | Format-Table -AutoSize}
 #>
 
-Write-Host "Queriying ALL printers on SERVER at $Date" -ForegroundColor DarkYellow
-    ForEach($ComputerName in $ComputerNames)   
-    {
-        Get-Printer -ComputerName $ComputerName | ForEach-Object {
-        Get-PrintJob -PrinterName $_.Name -ComputerName $ComputerName |
-        Where-Object { $_.JobStatus -like '*Error*'
-        } 
-            }
-                }
+Write-Host "Queriying ALL printers on $ComputerNames at $Date" -ForegroundColor DarkYellow
+ForEach($ComputerName in $ComputerNames)
+{Get-Printer -ComputerName $ComputerName | ForEach-Object {Get-PrintJob -PrinterName $_.Name -ComputerName $ComputerName | Where-Object { $_.JobStatus -like '*Error*'}}}
