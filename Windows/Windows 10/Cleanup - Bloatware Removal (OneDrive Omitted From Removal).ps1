aa5c3cbdb2017da19ae4e182129c7e296c4d3c98
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-20 09:30:16 -0400

    Update 2022-10-20

--- Windows/Windows 10/Cleanup - Bloatware Removal (OneDrive Omitted From Removal).ps1
+++ Windows/Windows 10/Cleanup - Bloatware Removal (OneDrive Omitted From Removal).ps1
@@ -270,6 +270,22 @@ New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Desktop' -Name 'ForegroundLoc
 New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Desktop' -Name 'HungAppTimeout' -Value '400' -PropertyType String -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Desktop' -Name 'WaitToKillAppTimeout' -Value '500' -PropertyType String -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control' -Name 'WaitToKillServiceTimeout' -Value '500' -PropertyType String -Force -ea SilentlyContinue;
+# Add "Open with Powershell" to right click menu
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+if((Test-Path -LiteralPath "HKCR:\Directory\Shell\PowershellMenu") -ne $true) {  New-Item "HKCR:\Directory\Shell\PowershellMenu" -Force | Out-Null};
+New-ItemProperty -LiteralPath "HKCR:\Directory\Shell\PowershellMenu" -Name "(Default)" -Value "Open with PowerShell (Admin)" -Force  | Out-Null
+Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\PowershellMenu" -Name "(Default)" -Value "Open with PowerShell (Admin)" -Force | Out-Null
+if((Test-Path -LiteralPath "HKCR:\Directory\Shell\PowershellMenu\command") -ne $true) {  New-Item "HKCR:\Directory\Shell\PowershellMenu\command" -Force -ErrorAction SilentlyContinue };
+Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\PowershellMenu\command" -Name "(Default)" -Value "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -NoExit -Command Set-Location -LiteralPath '%L'" -Force | Out-Null
+Remove-PSDrive HKCR
+# Add "Open with Command Prompt" to right click menu
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+if((Test-Path -LiteralPath "HKCR:\Directory\Shell\CMDMenu") -ne $true) {  New-Item "HKCR:\Directory\Shell\CMDMenu" -Force | Out-Null};
+New-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Prompt (Admin)" -Force  | Out-Null
+Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Prompt (Admin)" -Force | Out-Null
+if((Test-Path -LiteralPath "HKCR:\Directory\Shell\CMDMenu\command") -ne $true) {  New-Item "HKCR:\Directory\Shell\CMDMenu\command" -Force -ErrorAction SilentlyContinue };
+Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu\command" -Name "(Default)" -Value 'cmd.exe /s /k pushd "%V"' -Force | Out-Null
+Remove-PSDrive HKCR
 
 
 Write-Output "6. Performance"
