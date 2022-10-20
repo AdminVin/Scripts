c42524627f063ed765c6d640c21d37caf4c4824b
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-10-20 09:59:46 -0400

    Update 2022-10-20

--- Windows/Windows 11/Cleanup - Bloatware Removal.ps1
+++ Windows/Windows 11/Cleanup - Bloatware Removal.ps1
@@ -291,8 +291,8 @@ Write-Host "5.3 Added 'Open with PowerShell' to right click menu" -ForegroundCol
 # Add "Open with Command Prompt" to right click menu
 New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
 if((Test-Path -LiteralPath "HKCR:\Directory\Shell\CMDMenu") -ne $true) {  New-Item "HKCR:\Directory\Shell\CMDMenu" -Force | Out-Null};
-New-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Prompt (Admin)" -Force  | Out-Null
-Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Prompt (Admin)" -Force | Out-Null
+New-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Prompt" -Force  | Out-Null
+Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu" -Name "(Default)" -Value "Open with Command Promp" -Force | Out-Null
 if((Test-Path -LiteralPath "HKCR:\Directory\Shell\CMDMenu\command") -ne $true) {  New-Item "HKCR:\Directory\Shell\CMDMenu\command" -Force -ErrorAction SilentlyContinue };
 Set-ItemProperty -LiteralPath "HKCR:\Directory\Shell\CMDMenu\command" -Name "(Default)" -Value 'cmd.exe /s /k pushd "%V"' -Force | Out-Null
 Remove-PSDrive HKCR
