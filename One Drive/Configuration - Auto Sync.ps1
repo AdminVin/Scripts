23230c2a861082d02369992bdb1deef5042c24c8
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-11-04 14:48:51 -0400

    Update 2022-11-04

--- One Drive/Configuration - Auto Sync.ps1
+++ One Drive/Configuration - Auto Sync.ps1
@@ -3,6 +3,8 @@ When creating this with a GPO, create a BAT file and use the code below.
 Powershell.exe -ExecutionPolicy Bypass -File "\\SERVER\SHARE\PowerShell_Script.ps1"
 GPO (Computer > Startup Script)
 #>
+# Tenant ID can be located Portal.Azure.com > Azure Active Directory > Overview
+$TenantGUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
 #region OneDrive - Auto Sync Desktop, Documents, and Pictures
 # Source: https://learn.microsoft.com/en-us/sharepoint/use-group-policy#silently-move-windows-known-folders-to-onedrive
