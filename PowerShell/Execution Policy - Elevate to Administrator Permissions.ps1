57356c96be80b56b693e4797d7b3b6f3661eb7de
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-11-15 14:23:12 -0500

    Update 2022-11-15

---
+++ PowerShell/Execution Policy - Elevate to Administrator Permissions.ps1
@@ -0,0 +1,2 @@
+Write-Host "Elevating Powershell Script with Administrative Rights" -ForegroundColor Green
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs exit }
