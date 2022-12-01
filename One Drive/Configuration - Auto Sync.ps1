6b7717cf00b96aa4dcd1df88035ffc7eea63af67
Vincent <1580359+AdminVin@users.noreply.github.com>
2022-12-01 15:40:09 -0500

    Update 2022-12-01

--- One Drive/Configuration - Auto Sync.ps1
+++ One Drive/Configuration - Auto Sync.ps1
@@ -3,8 +3,10 @@ When creating this with a GPO, create a BAT file and use the code below.
 Powershell.exe -ExecutionPolicy Bypass -File "\\SERVER\SHARE\PowerShell_Script.ps1"
 GPO (Computer > Startup Script)
 #>
+#region Tenant Information
 # Tenant ID can be located Portal.Azure.com > Azure Active Directory > Overview
 $TenantGUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#endregion
 
 #region OneDrive - Auto Sync Desktop, Documents, and Pictures
 # Source: https://learn.microsoft.com/en-us/sharepoint/use-group-policy#silently-move-windows-known-folders-to-onedrive
@@ -12,8 +14,14 @@ if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive") -ne $tr
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive' -Name 'KFMSilentOptIn' -Value $TenantGUID -PropertyType String -Force -ErrorAction SilentlyContinue;
 #endregion
 
-#region Preliminary Checks
+#region Settings
 # Re-Enable One Drive Usage/Syncing (if previously disabled)
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Value '0' -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null;
 Set-ItemProperty -Path "HKLM\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value "0" -ErrorAction SilentlyContinue | Out-Null;
+
+# Disable Syncing of Shortcuts Filetypes [Applications (.lnk) & Internet (.url)]
+# Useful for when shortcuts are pushed out via InTune/GPO and OneDrive will duplicate shortcuts if the user logs into multiple PCs.
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\EnableODIgnoreListFromGPO") -ne $true) {  New-Item "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\EnableODIgnoreListFromGPO" -force -ea SilentlyContinue }
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\EnableODIgnoreListFromGPO' -Name '*.lnk' -Value '*.lnk' -PropertyType String -Force -ErrorAction SilentlyContinue
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\EnableODIgnoreListFromGPO' -Name '*.url' -Value '*.url' -PropertyType String -Force -ErrorAction SilentlyContinue
 #endregion
--- One Drive/Configuration - Silent Setup.ps1
+++ One Drive/Configuration - Silent Setup.ps1
@@ -3,18 +3,21 @@ When creating this with a GPO, create a BAT file and use the code below.
 Powershell.exe -ExecutionPolicy Bypass -File "\\SERVER\SHARE\PowerShell_Script.ps1"
 GPO (User > Login Script)
 #>
+
+#region Tenant Information
+# Tenant ID can be located Portal.Azure.com > Azure Active Directory > Overview
+$TenantGUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#endregion
+
 #region Configure OneDrive to Silently Configure
 $HKLMregistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" # Path to HKLM keys
 $DiskSizeregistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\DiskSpaceCheckThresholdMB"# Path to max disk size key
-# Tenant ID can be located Portal.Azure.com > Azure Active Directory > Overview
-$TenantGUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-
 if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
 if(!(Test-Path $DiskSizeregistryPath)){New-Item -Path $DiskSizeregistryPath -Force}
-
 New-ItemProperty -Path $HKLMregistryPath -Name "SilentAccountConfig" -Value "1" -PropertyType DWORD -Force | Out-Null #Enable silent account configuration
 New-ItemProperty -Path $DiskSizeregistryPath -Name $TenantGUID -Value "102400" -PropertyType DWORD -Force | Out-Null #Set max OneDrive threshold before prompting
 #endregion
+
 
 #region OneDrive - FORCE Auto Start on Login
 # Varibles
