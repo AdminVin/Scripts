f24156e15b096083672eca665fcf8a1243064c38
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-01-27 09:38:21 -0500

    Update 2023-01-27

--- Windows/Remote Administration Tools.ps1
+++ Windows/Remote Administration Tools.ps1
@@ -6,3 +6,12 @@ Get-WindowsCapability -Online | ? Name -like "RSAT*"| Add-WindowsCapability -Onl
 
 # Install Active Directory
 Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+
+
+# Install Active Directory (behind WSUS Server)
+$currentWU = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" | select -ExpandProperty UseWUServer
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value 0
+Restart-Service wuauserv
+Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value $currentWU
+Restart-Service wuauserv
