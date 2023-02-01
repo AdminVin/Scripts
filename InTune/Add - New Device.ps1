1fa15a58718390a4ad745972e7afc86b760e1f11
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-02-01 10:22:45 -0500

    Update 2023-02-01

---
+++ InTune/Add - New Device.ps1
@@ -0,0 +1,15 @@
+# After reimage on language selection hold Shift + press F10
+
+PowerShell.exe -ExecutionPolicy Bypass
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Get-WindowsAutoPilotInfo -Online
+# Login to the Microsoft window prompt (if you do not see it alt+tab to bring it to the front)
+
+<# Output:
+Installing module WindowsAutopilotIntune
+Connected to Intune tenant xxxxxxxxxxxx-xxx-xxx-xxxxxxxxx
+Gathered details for device with serial number: xxxxxx
+Waiting for 1 of 1 to be imported
+#>
+
+# Wait 5 minutes, and log into Intune -> Devices -> Windows -> Windows Enrollment -> Devices -> Search for Device Serial Number > Click SYNC button at top of screen.
