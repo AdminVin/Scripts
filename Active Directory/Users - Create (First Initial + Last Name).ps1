cc7d3b71379f36cb0fa9762d400e2d967be0001a
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-02-10 09:16:41 -0500

    Update 2023-02-10

--- Active Directory/Users - Create (First Initial + Last Name).ps1
+++ Active Directory/Users - Create (First Initial + Last Name).ps1
@@ -1,20 +1,12 @@
-# Import active directory module for running AD cmdlets
+## Settings
 Import-Module ActiveDirectory
-  
-#Store the data from ADUsers.csv in the $ADUsers variable
-$ADUsers = Import-csv ".\Users - Create (First Initial + Last Name).csv"
-# Update to Pulic Domain Name
+$CSV = Import-CSV ".\Users - Create (First Initial + Last Name).csv"
 $EmailDomain = "AdminVin.com"
-# OU that the users will be created in
-#$OU = "OU=Users,OU=Company,OU=ParentCompany,DC=AV,DC=local"
 $OU = "OU=Users,OU=Company,OU=ParentCompany,DC=AV,DC=local"
 
-
-#Loop through each row containing user details in the CSV file 
-foreach ($User in $ADUsers)
+## Process Accounts
+foreach ($User in $CSV)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
-		
 	$Username 	= $User.FirstName.Substring(0,1)+$User.LastName
 	$Password 	= $User.Password
 	$FirstName 	= $User.FirstName
@@ -29,42 +21,14 @@ foreach ($User in $ADUsers)
     $State = $User.State
     $Zip = $User.Zip
     $City = $User.City
+    $UPN = $Username+"@"+$EmailDomain
  
-	#Check to see if the user already exists in AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 #If user does exist, give a warning
 		 Write-Warning "A User $Username already exist in Active Directory."
     }
-
 	else
-
 	{
-		#User does not exist then proceed to create the new user account
-		$UPNMAin = $Username+"@"+$EmailDomain
-        #Account will be created in the OU provided by the $OU variable read from the CSV file
-		New-ADUser `
-            -SamAccountName $Username `
-            -UserPrincipalName $UPNMAin ` `
-            -Name "$FirstName $LastName" `
-            -GivenName $FirstName `
-            -Surname $LastName `
-            -Enabled $True `
-            -DisplayName "$FirstName $LastName" `
-            -Initials $MiddleInitial `
-            -Path $OU `
-            -Title $JobTitle `
-            -Company $CompayName `
-            -StreetAddress $Street `
-            -Email $UPNMAin `
-            -MobilePhone $Cell `
-            -Fax $Fax `
-            -OfficePhone $DirectLine `
-            -State $State `
-            -City $City `
-            -PostalCode $Zip `
-            -AccountPassword (convertto-securestring $Password -AsPlainText -Force) `
-            -OtherAttributes @{Proxyaddresses="SMTP:"+$UPNMain}
+		New-ADUser -SamAccountName $Username -UserPrincipalName $UPN -Name "$FirstName $LastName" -GivenName $FirstName -Surname $LastName -DisplayName "$FirstName $LastName" -Initials $MiddleInitial -Title $JobTitle -Company $CompayName -StreetAddress $Street -Email $UPN -MobilePhone $Cell -Fax $Fax -OfficePhone $DirectLine -State $State -City $City -PostalCode $Zip -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -OtherAttributes @{Proxyaddresses="SMTP:"+$UPN} -Path $OU -Enabled $True -VERBOSE
     }
-    # Set-ADUser -Identity $user.samaccountname -Email $user.UPNMAin
-}
+ }
--- Active Directory/Users - Create (Generic).ps1
+++ Active Directory/Users - Create (Generic).ps1
@@ -1,31 +1,24 @@
-# Import AD
+## Settings
 Import-Module ActiveDirectory
-# Import CSV
 $CSV = Import-CSV "Users - Create (Generic).csv" -Verbose
-# Update to Pulic Domain Name
 $EmailDomain = "AdminVin.com"
-# OU that the users will be created in
 $OU = "OU=Users,OU=Company,OU=ParentCompany,DC=AV,DC=local"
 
-
-#Loop through each row containing user details in the CSV file 
+## Process Accounts
 foreach ($User in $CSV)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
 	$Username = $User.AccountName
 	$Password = $User.Password
 	$FirstName = $User.FirstName
 	$Lastname = $User.LastName
 	$UPNMAin = $Username+"@"+$EmailDomain
  
-	#Check to see if the user already exists in AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
 		 Write-Warning "A User $Username already exist in Active Directory."
     }
 	else
 	{
-		#User does not exist then proceed to create the new user account
 	    New-ADUser -SamAccountName $Username -UserPrincipalName $UPNMAin -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -GivenName $FirstName -Surname $LastName -Name "$FirstName $LastName" -DisplayName "$FirstName $LastName" -Email $UPNMAin -OtherAttributes @{Proxyaddresses="SMTP:"+$UPNMain} -Path $OU -Enabled $True -Verbose       
     }
 }
