074a8a593fd4356f27e8aa19bbe36a3b9a4e902d
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-02-16 13:44:52 -0500

    Update 2023-02-16

--- Printers/Queue - Job Errors - Removal.ps1
+++ Printers/Queue - Job Errors - Removal.ps1
@@ -2,11 +2,6 @@
 $PrintServer = "SERVER_NAME"
 # Print Spooler must be running on the remote system running this script.
 
-<#
-# View Expired Print Jobs
-Get-Printer -ComputerName $PrintServer | Get-Printjob | Where-Object {$_.jobstatus -like "*Error*" } | Select-Object @{name="Name";expression={$_.printerName}}, @{name="Submitted Time";expression={$_.SubmittedTime}}, jobstatus, PrinterName, @{name="Document Name";expression={$_.documentname}} | Sort-Object -Property jobstatus -Descending
-#>
-
 # Remove Expired Print Jobs
 Get-Printer -ComputerName $PrintServer | Get-Printjob | Where-Object {$_.jobstatus -like "*Error*" } | Remove-PrintJob
 # Restart Print Spooler
