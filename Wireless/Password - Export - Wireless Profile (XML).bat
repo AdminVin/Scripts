7912f96d278da09513e016d2c7d607ac3ed585f2
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-07 16:16:20 -0500

    Update 2023-03-07

--- Wireless/Password - Export - Wireless Profile (XML).bat
+++ Wireless/Password - Export - Wireless Profile (XML).bat
@@ -10,3 +10,12 @@ netsh wlan export profile name="%Wifiprofilename%" folder="C:\" key=clear
 
 :: Import profile to another computer
 :: netsh wlan add profile filename="%Wifiprofilename%.XML" user=all
+
+:: If your wireless password has special characters in it, they will have to be replaced with the legend below or will fail to add the profile properly.
+:: " (change to) &quot;
+:: & (change to) &amp;
+:: ' (change to) &apos;
+:: < (change to) &lt; 
+:: > (change to) &gt;
+:: 
+:: Example: P@ssw0rd&123 should be P@ssw0rd&amp;123
