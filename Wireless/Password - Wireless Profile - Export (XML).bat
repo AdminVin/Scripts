05dca61d75fc02b501e57cb86abd082b95150366
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-08 08:39:01 -0500

    Update 2023-03-08

--- Wireless/Password - Wireless Profile - Export (XML).bat
+++ Wireless/Password - Wireless Profile - Export (XML).bat
@@ -1,7 +1,7 @@
 :: Identify & reveal wireless password
 netsh wlan show profile
 echo *
-set /p Wifiprofilename=Enter Wifi profile name you would like to see the password for (NOTE: Wifi profilesare case sensitive):
+set /p Wifiprofilename=Enter Wifi profile name you would like to see the password for (NOTE: Wifi profiles are case sensitive):
 netsh wlan show profile "%Wifiprofilename%" key=clear
 pause
 
