5c7cbd2a975c132faaf2fe9015f6c7464d4bb63d
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-16 10:26:25 -0400

    Update 2023-03-16

--- Office 365/Connect to Office 365.ps1
+++ Office 365/Connect to Office 365.ps1
@@ -1,9 +1,8 @@
 ### MFA Enabled Office 365 Tenants
 # Prerequisities
-Set-ExecutionPolicy Unrestricted
-Install-Module MSOnline
-Install-Module AzureAD
-Install-Module ExchangeOnlineManagement
+IF(!(Get-Module -Name AzureAD -ListAvailable)){Install-Module -Name AzureAD -Scope CurrentUser -Force;Import-Module AzureAD;Write-Host "AzureAD"} ELSE {Import-Module AzureAD;Write-Host "AzureAD"}
+IF(!(Get-Module -Name ExchangeOnlineManagement -ListAvailable)){Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser -Force;Import-Module ExchangeOnlineManagement;Write-Host "ExchangeOnlineManagement"} ELSE {Import-Module ExchangeOnlineManagement;Write-Host "ExchangeOnlineManagement"}
+IF(!(Get-Module -Name MSOnline -ListAvailable)){Install-Module -Name MSOnline -Scope CurrentUser -Force;Import-Module MSOnline;Write-Host "MSOnline"} ELSE {Import-Module MSOnline;Write-Host "MSOnline"}
 
 # Connect
 Connect-EXOPSSession
