3ba946479fc1cf0ff8e87120f2e040783b6f8038
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-20 13:55:40 -0400

    Update 2023-03-20

--- PowerShell/Progress Bar - Start-Sleep.ps1
+++ PowerShell/Progress Bar - Start-Sleep.ps1
@@ -6,5 +6,6 @@ For ($Time; $Time -gt 0; $Time--) {
 $min = [int](([string]($Time/60)).split('.')[0])
 $text = " " + $min + " minutes " + ($Time % 60) + " seconds left"
 Write-Progress -Activity "Watiting for..." -Status $Text -PercentComplete ($Time / $Lenght)
-Start-Sleep 1 }
+Start-Sleep 1
+Write-Progress -Activity "Watiting for..." -Status "Ready" -Completed }
 #endregion
