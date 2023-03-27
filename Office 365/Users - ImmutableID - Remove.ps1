4561b2af0a8556ac8fb031e4482a5cf8528df465
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-27 10:48:13 -0400

    Update 2023-03-27

--- Office 365/Users - ImmutableID - Remove.ps1
+++ Office 365/Users - ImmutableID - Remove.ps1
@@ -1,4 +1,4 @@
 # This is for removing the ImmutableID and to attempt to DirSync the user to the in cloud account
 #
 Connect-MsolService
-Set-MSOLUser -UserPrincipalName user@DOMAIN.com -ImmutableID "$null"
+Set-MSOLUser -UserPrincipalName user@DOMAIN.com -ImmutableID $null
