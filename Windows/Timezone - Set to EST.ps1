c5c4d26a30450ec69f1783b02f9351d75e4e971f
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-03-31 09:53:53 -0400

    Update 2023-03-31

--- Windows/Timezone - Set to EST.ps1
+++ Windows/Timezone - Set to EST.ps1
@@ -1,3 +1,13 @@
-Set-TimeZone -Name 'Eastern Standard Time'
+# Set Eastern Time Zone
+Set-TimeZone -Name "Eastern Standard Time"
 Start-Service W32Time
 Restart-Service W32Time
+
+# Get the current date and time from an internet time server
+$ntpServer = "time.google.com"
+$dateTime = (Get-Date -UFormat %s)
+$ntpResult = Invoke-WebRequest -Uri "http://$ntpServer" -Method Get -UseBasicParsing
+$ntpTime = [DateTime]::Parse($ntpResult.Headers.Date)
+
+# Set the system date and time
+Set-Date $ntpTime
