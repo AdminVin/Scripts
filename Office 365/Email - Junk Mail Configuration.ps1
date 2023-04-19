e922f41e8e51394aa100daffe2a86508f75559c9
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-04-19 10:20:07 -0400

    Update 2023-04-19

--- Office 365/Email - Junk Mail Configuration.ps1
+++ Office 365/Email - Junk Mail Configuration.ps1
@@ -3,3 +3,6 @@ Get-MailboxJunkEmailConfiguration user@DOMAIN.com
 
 # Disable Junk Mail Filter
 Set-MailboxJunkEmailConfiguration user@DOMAIN.com -Enabled $false
+
+# Enable Junk Mail Filter (Default)
+Set-MailboxJunkEmailConfiguration user@DOMAIN.com -Enabled $true
