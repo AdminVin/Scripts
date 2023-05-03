275a2b7d4a2badeb1dd2e21d41e66c7e8ea1bb48
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-05-03 16:08:34 -0400

    Update 2023-05-03

--- Office 365/Email - Junk Mail.ps1
+++ Office 365/Email - Junk Mail.ps1
@@ -6,3 +6,15 @@ Set-MailboxJunkEmailConfiguration user@DOMAIN.com -Enabled $false
 
 # Enable Junk Mail Filter (Default)
 Set-MailboxJunkEmailConfiguration user@DOMAIN.com -Enabled $true
+
+
+############################
+# NOTE #
+########
+
+# This command will technically work in Office 365.  However,the Anti-Spam policy will still dicate what goes into the junk mail folder regardless.
+# Source: https://learn.microsoft.com/en-us/powershell/module/exchange/set-mailboxjunkemailconfiguration?view=exchange-ps
+
+<#
+ In Exchange Online, the safelist collection of the mailbox is unable to move messages between the Inbox and the Junk Email folder. Messages are still delivered to the Junk Email folder based on the verdict and corresponding action of anti-spam policies.
+#>
