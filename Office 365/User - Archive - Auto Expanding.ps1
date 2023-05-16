8189d5fa675a27c17d9bfd3c8c59e62532a22d39
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-05-16 12:12:05 -0400

    Update 2023-05-16

---
+++ Office 365/User - Archive - Auto Expanding.ps1
@@ -0,0 +1,12 @@
+# User will need have the licensing Exchange Online 2 at minimum.
+
+# Install/Import Module
+Install-Module -Name ExchangeOnlineManagement
+Import-Module ExchangeOnlineManagement
+# Connect to Office 365
+Connect-ExchangeOnline
+
+# Enable Auto Expanding Archive
+Enable-Mailbox USER@DOMAIN.com -AutoExpandingArchive
+# Force start auto expanding archive on the mailbox immediately, rather than wait to start on the next scheduled time.
+Start-ManagedFolderAssistant -Identity USER@DOMAIN.com
