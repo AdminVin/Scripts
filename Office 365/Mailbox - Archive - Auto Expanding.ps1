d3dcdf9d54b0fc617bad584a1e2637e554f332e1
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-05-17 10:20:25 -0400

    Update 2023-05-17

--- Office 365/Mailbox - Archive - Auto Expanding.ps1
+++ Office 365/Mailbox - Archive - Auto Expanding.ps1
@@ -10,3 +10,6 @@ Connect-ExchangeOnline
 Enable-Mailbox USER@DOMAIN.com -AutoExpandingArchive
 # Force start auto expanding archive on the mailbox immediately, rather than wait to start on the next scheduled time.
 Start-ManagedFolderAssistant -Identity USER@DOMAIN.com
+
+# Verify Auto Expanding Archive is Enabled
+Get-Mailbox USER@DOMAIN.com | Format-List AutoExpandingArchiveEnabled
