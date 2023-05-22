6fd8c293fc42acad25664221e3141b9cfae86a32
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-05-22 15:39:34 -0400

    Update 2023-05-22

--- Google Domains/Dynamic DNS - Update Domain.sh
+++ Google Domains/Dynamic DNS - Update Domain.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Set the API user ID and password
-apiUserID="XXXXX"                # Google Domains - API Username
-apiPassword="XXXXX"              # Google Domains - API Password
-
-# Function to display a progress bar, live countdown, and timer
+# Functions
 function show_progress_bar() {
     local duration=$1
     local progress=0
@@ -28,23 +24,21 @@ function show_progress_bar() {
 }
 
 while true; do
-    # Get the current public IP address
-    ipAddress=$(curl -s https://api.ipify.org)
-
-    # Set the domain name and host
-    domain="DOMAIN.COM"                    # Google Domain being updated (example: domain="MyDomain.com")
-    host="@"
-
-    # Update the dynamic DNS
+    ## Domain
+    domain="DOMAIN.COM"                         # Google Domain being updated (example: domain="MyDomain.com")
+    host="@"                                    # @ being A Record for DOMAIN.com
+    # API Credentials
+    apiUserID="XXXXX"                           # Google Domains - API Username
+    apiPassword="XXXXX"                         # Google Domains - API Password
+    # IP Address - Pull Current
+    ipAddress=$(curl -s https://api.ipify.org) 
+    # Google Domains - Update
     response=$(curl -s "https://domains.google.com/nic/update?hostname=$host.$domain&myip=$ipAddress" \
               --user "$apiUserID:$apiPassword" \
               --silent \
               --show-error)
-
-    # Print the response
-    echo $response
-
-    # Show a progress bar, live countdown, and timer for 30 minutes (1800 seconds) sleep
+    # Display Results
+    echo "$domain: $response"                   # Example Output: DOMAIN.com: nochg 1.2.3.4
+    # Sleep 30 Minutes
     show_progress_bar 1800
-
 done
