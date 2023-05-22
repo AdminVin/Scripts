169a8cbb7b176324bf9da65d802a7ff765972a3e
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-05-22 13:46:06 -0400

    Update 2023-05-22

--- Google Domains/Dynamic DNS - Update Domain.sh
+++ Google Domains/Dynamic DNS - Update Domain.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
 # Set the API user ID and password
-apiUserID="xxxxxxxxx"
-apiPassword="xxxxxxxxx"
+apiUserID="XXXXX"                # Google Domains - API Username
+apiPassword="XXXXX"              # Google Domains - API Password
 
-# Get the current public IP address
-ipAddress=$(curl -s https://api.ipify.org)
+# Function to display a progress bar, live countdown, and timer
+function show_progress_bar() {
+    local duration=$1
+    local progress=0
+    local progress_char="#"
+    local empty_char="-"
+    local bar_length=20
 
-# Set the domain name and host
-domain="example.com"
-host="@"
+    while [ $progress -lt $duration ]; do
+        remaining=$((duration - progress))
+        mins=$((remaining / 60))
+        secs=$((remaining % 60))
+        printf "Progress: ["
+        printf "%-$((progress * bar_length / duration))s" | tr ' ' $progress_char
+        printf "%-$(((duration - progress) * bar_length / duration))s" | tr ' ' $empty_char
+        printf "] Next Sync: %02d:%02d " "$mins" "$secs"
+        sleep 1
+        printf "\r"
+        progress=$((progress + 1))
+    done
+    printf "\n"
+}
 
-# Update the dynamic DNS
-response=$(curl -s "https://domains.google.com/nic/update?hostname=$host.$domain&myip=$ipAddress" \
-          --user "$apiUserID:$apiPassword" \
-          --silent \
-          --show-error)
+while true; do
+    # Get the current public IP address
+    ipAddress=$(curl -s https://api.ipify.org)
 
-# Print the response
-echo $response
+    # Set the domain name and host
+    domain="DOMAIN.COM"                    # Google Domain being updated (example: domain="MyDomain.com")
+    host="@"
+
+    # Update the dynamic DNS
+    response=$(curl -s "https://domains.google.com/nic/update?hostname=$host.$domain&myip=$ipAddress" \
+              --user "$apiUserID:$apiPassword" \
+              --silent \
+              --show-error)
+
+    # Print the response
+    echo $response
+
+    # Show a progress bar, live countdown, and timer for 30 minutes (1800 seconds) sleep
+    show_progress_bar 1800
+
+done
