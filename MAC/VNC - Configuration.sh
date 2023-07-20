1f82b8eea0ed50eb0bf4b952af97ccd1a51dfdc9
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-07-20 15:59:17 -0400

    Update 2023-07-20

--- MAC/VNC - Configuration.sh
+++ MAC/VNC - Configuration.sh
@@ -1,7 +1,5 @@
-# Enable and Configure
+# Enable, configure, and restart service
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -clientopts -setvnclegacy -vnclegacy yes -clientopts -setvncpw -vncpw YOUR_PASSWORD
-
-# Restart Service
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
