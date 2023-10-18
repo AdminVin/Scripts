d622e2811b1d5a6f5c5fa5088f7448b7e13880c1
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-10-18 09:32:50 -0400

    Update 2023-10-18

---
+++ Office 365/Users - Last Login Status.ps1
@@ -0,0 +1,33 @@
+# Notes
+# This is for Active Directory accounts synced to Office 365.
+
+# Modules
+Connect-ExchangeOnline
+
+# Active Directory - Check all enabled users with "Staff" in ExtensionAttribute1
+$ActiveUsers = Get-ADUser -Filter {Enabled -eq $true}
+
+# Initialize an array to store user data
+$UserData = @()
+
+# Office 365 - Check Last Interaction Time
+foreach ($O365user in $ActiveUsers) {
+  $O365userUPN = $O365user.UserPrincipalName
+  Write-Host "Checking $O365userUPN"
+  $O365Status = Get-MailboxStatistics $O365user.UserPrincipalName | Select-Object LastInteractionTime
+  $UserDataRow = New-Object PSObject -Property @{
+    "User" = $O365user.Name
+    "UserPrincipalName" = $O365user.UserPrincipalName
+    "LastInteractionTime" = $O365Status.LastInteractionTime
+  }
+  $UserData += $UserDataRow
+}
+
+# Define the CSV file path
+$CsvFilePath = "C:\Users - Last Login Status.csv"
+
+# Export the data to a CSV file
+$UserData | Export-Csv -Path $CsvFilePath -NoTypeInformation
+
+# Notify
+Write-Host "User data has been exported to $CsvFilePath"
