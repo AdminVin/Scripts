0af790c2190abc79332b3bd543f71a9baf1545bb
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-11-07 08:18:30 -0500

    Update 2023-11-07

--- MAC/VNC - Configuration.sh
+++ MAC/VNC - Configuration.sh
@@ -1,5 +1,9 @@
-# Enable, configure, and restart service
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -clientopts -setvnclegacy -vnclegacy yes -clientopts -setvncpw -vncpw PASSWORD
+# Enable/Configure/Restart Service
+
+# Set with VNC password, and use accound creds to access remote machine.
+#sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -clientopts -setvnclegacy -vnclegacy yes -clientopts -setvncpw -vncpw PASSWORD
+# Set no VNC passwords and only use account creds to access remote machine.
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -clientopts -setvnclegacy -vnclegacy yes -clientopts
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
