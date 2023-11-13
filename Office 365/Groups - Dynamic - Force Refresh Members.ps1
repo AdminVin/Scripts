e5937e538fed62b418da9ea2560b99aa45b88d71
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-11-13 09:40:14 -0500

    Update 2023-11-13

--- Office 365/Groups - Dynamic - Force Refresh Members.ps1
+++ Office 365/Groups - Dynamic - Force Refresh Members.ps1
@@ -1,1 +1,4 @@
+# Dynamic distribution groups can take up to twenty four hours to update as per Microsoft documentation.
+# Source: https://learn.microsoft.com/en-us/exchange/recipients-in-exchange-online/manage-dynamic-distribution-groups/modern-dynamic-distribution-groups
+
 Set-DynamicDistributionGroup -Identity "DynamicGroupName@DOMAIN.com" -ForceMembershipRefresh
