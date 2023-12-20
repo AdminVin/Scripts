adf507923fe06909a8d975440450e23a51a3d941
Vincent <1580359+AdminVin@users.noreply.github.com>
2023-12-20 14:57:28 -0500

    Update 2023-12-20

--- Wireless/Password - Wireless Profile - Export (XML).bat
+++ Wireless/Password - Wireless Profile - Export (XML).bat
@@ -1,9 +1,18 @@
+echo off
 :: Identify & reveal wireless password
 netsh wlan show profile
 echo *
 set /p Wifiprofilename=Enter Wifi profile name you would like to see the password for (NOTE: Wifi profiles are case sensitive):
 netsh wlan show profile "%Wifiprofilename%" key=clear
-pause
 
 :: Export wireless profile to .XML
 netsh wlan export profile name="%Wifiprofilename%" folder="C:\" key=clear
+
+:: Notify
+echo " "
+echo "***********************************************************"
+echo "Wireless Profile exported to C:\"
+pause
+
+:: If the above pulls the password encrypted, it will only work on your PC to add.
+:: You can alternatively pull the password with NetSetMan Pro - https://www.netsetman.com/
