dabc04029dcc0dbd04e9848492308954e257a010
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-02-23 14:31:29 -0500

    Update 2024-02-23

--- Windows/Windows Update - P1 - Install.ps1
+++ Windows/Windows Update - P1 - Install.ps1
@@ -1,9 +1,12 @@
 ## Setup Notes
 # Login to server > Scheduled Tasks > Create New Task (not basic)
-# Schedule:  Tues/Weds @ 6 PM 
+# Schedule:  Weds @ 6 PM 
 # Action > New
 #       Program/Script: Point to PS7 (C:\Program Files\PowerShell\7\pwsh.exe)
 #         Argument: -ExecutionPolicy Bypass -File "UNC or Local Path to this PS Script."
+#
+# NOTE: Script will need to be run once manually to enter in password for Office 365 account.
+
 
 ## PowerShell - Modules
 # NuGet
@@ -28,3 +31,41 @@ if (-not $module) {
 Get-WindowsUpdate
 # Install
 Install-WindowsUpdate -AcceptAll -Confirm:$false -IgnoreReboot
+
+
+## Email
+# Body
+# Windows Update - All
+$WUHistoryRaw = Get-WUHistory | Where-Object { $_.Date -ge (Get-Date).AddDays(-2) }
+# Windows Update - Failed
+$FailedUpdatesExist = $WUHistoryRaw | Where-Object { $_.Result -ne "Succeeded" }
+$StatusIndicator = if ($FailedUpdatesExist) { "REVIEW" } else { "SUCCESS" }
+# Convert to HTML
+IF ($StatusIndicator -eq "SUCCESS") {
+    $WUHistoryHtml = $WUHistoryRaw | Select-Object Date, Result, Title | ConvertTo-Html -Head "<style>table {border-collapse: collapse;} th, td {border: 1px solid black; padding: 5px;}</style>" -Body "<h2>Windows Update History</h2><b>$env:COMPUTERNAME has installed the updates below.</b><br><br><br>" | Out-String
+} else {
+    $WUHistoryHtml = $WUHistoryRaw | Select-Object Date, Result, Title | ConvertTo-Html -Head "<style>table {border-collapse: collapse;} th, td {border: 1px solid black; padding: 5px;}</style>" -Body "<h2>Windows Update History</h2><b>$env:COMPUTERNAME has attempted to install the updates below, but the updates need to be reviewed.</b><br><br><br>" | Out-String
+}
+# Subject
+$EmailSubject = "Windows Updates - $env:COMPUTERNAME ($StatusIndicator)"
+# Email Server Settings and Credentials
+$SMTPUsername = "alerts@DOMAIN.COM"
+$CUfile = $SMTPUsername + ".txt"
+$Directory = "C:\ProgramData\AV\WindowsUpdates"
+# Password
+IF(Test-Path "$Directory\$CUfile") {
+    # Password file exists
+} ELSE {
+    New-Item -ItemType Directory -Path $Directory -Force | Out-Null
+    Read-Host -Prompt "Enter password for $SMTPUsername to be encrypted & used for sending alerts" -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "$Directory\$CUfile"
+}
+$SecureStringContent = Get-Content -Path "$Directory\$CUfile" | ConvertTo-SecureString
+$SMTPPassword = $SecureStringContent
+$SMTPCredential = New-Object System.Management.Automation.PSCredential ($SMTPUsername, $SMTPPassword)
+# SMTP Server Settings
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = 587
+$EmailFrom = $SMTPUsername
+$EmailTo = "ITADMINS@DOMAIN.COM"
+# Send Email
+if (!($WUHistoryRaw -eq '')) {Send-MailMessage -SmtpServer $SMTPServer -Port $SMTPPort -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Body $WUHistoryHtml -Credential $SMTPCredential -UseSsl -BodyAsHtml}
--- Windows/Windows Update - P2 - Reboot.ps1
+++ Windows/Windows Update - P2 - Reboot.ps1
@@ -4,6 +4,9 @@
 # Action > New
 #       Program/Script: Point to PS7 (C:\Program Files\PowerShell\7\pwsh.exe)
 #         Argument: -ExecutionPolicy Bypass -File "UNC or Local Path to this PS Script."
+#
+# NOTE: Script will need to be run once manually to enter in password for Office 365 account.
+
 
 # Reboot Check
 $rebootRequired = $false
@@ -13,12 +16,35 @@ if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Aut
 
 # Reboot PC
 if ($rebootRequired) {
-    # Reboot required.
-    $Directory = "C:\ProgramData\EBPS\WindowsUpdates"
-    $File = Join-Path -Path $Directory -ChildPath "RebootNeeded.txt"
-    if (-not (Test-Path $Directory)) {
-        New-Item -ItemType Directory -Path $Directory -Force
+    ## Email
+    # Body
+    # Windows Update - All
+    $WUHistoryRaw = Get-WUHistory | Where-Object { $_.Date -ge (Get-Date).AddDays(-2) }
+    # Convert to HTML for Email Body
+    $WUHistoryHtml = $WUHistoryRaw | Select-Object Date, Result, Title | ConvertTo-Html -Head "<style>table {border-collapse: collapse;} th, td {border: 1px solid black; padding: 5px;}</style>" -Body "<h2>Windows Update History</h2><b>$env:COMPUTERNAME is rebooting to complete the updates below.</b><br><br><br>" | Out-String
+    # Subject
+    $EmailSubject = "Windows Updates - $env:COMPUTERNAME (REBOOT)"
+    # Email Server Settings and Credentials
+    $SMTPUsername = "alerts@DOMAIN.COM"
+    $CUfile = $SMTPUsername + ".txt"
+    $Directory = "C:\ProgramData\AV\WindowsUpdates"
+    # Password
+    IF(Test-Path "$Directory\$CUfile") {
+        # Password file exists
+    } ELSE {
+        New-Item -ItemType Directory -Path $Directory -Force | Out-Null
+        Read-Host -Prompt "Enter password for $SMTPUsername to be encrypted & used for sending alerts" -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "$Directory\$CUfile"
     }
-    New-Item -ItemType File -Path $File -Force | Out-Null
+    $SecureStringContent = Get-Content -Path "$Directory\$CUfile" | ConvertTo-SecureString
+    $SMTPPassword = $SecureStringContent
+    $SMTPCredential = New-Object System.Management.Automation.PSCredential ($SMTPUsername, $SMTPPassword)
+    # SMTP Server Settings
+    $SMTPServer = "smtp.office365.com"
+    $SMTPPort = 587
+    $EmailFrom = $SMTPUsername
+    $EmailTo = "ITADMINS@DOMAIN.COM"
+    # Send Email
+    if (!($WUHistoryRaw -eq '')) {Send-MailMessage -SmtpServer $SMTPServer -Port $SMTPPort -From $EmailFrom -To $EmailTo -Subject $EmailSubject -Body $WUHistoryHtml -Credential $SMTPCredential -UseSsl -BodyAsHtml}
+    ## Reboot
     Restart-Computer -Force
 }
