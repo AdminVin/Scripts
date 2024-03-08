0dfa17316add39c9591ce33d8af91d2a344462e4
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-03-08 11:09:20 -0500

    Update 2024-03-08

--- Office 365/Mailbox - Archive (Online, Expand, Holds).ps1
+++ Office 365/Mailbox - Archive (Online, Expand, Holds).ps1
@@ -1,25 +1,22 @@
+### Prerequisites 
 # User will need have the licensing Exchange Online 2 at minimum.
 
-# Install/Import Module/Connect
-Install-Module -Name ExchangeOnlineManagement
-Import-Module ExchangeOnlineManagement
+
+### Connect to Office 365
 Connect-ExchangeOnline
 
-# Enable Auto Expanding Archive
-Enable-Mailbox USER@DOMAIN.com -AutoExpandingArchive
-# Verify Auto Expanding Archive is Enabled
-Get-Mailbox USER@DOMAIN.com | Format-List AutoExpandingArchiveEnabled
 
-# Force start auto expanding archive on the mailbox immediately, rather than wait to start on the next scheduled interval.
-Start-ManagedFolderAssistant -Identity USER@DOMAIN.com -FullCrawl
+## Auto Expanding Archive
+Enable-Mailbox USER@DOMAIN.com -AutoExpandingArchive                            # Enable
+Get-Mailbox USER@DOMAIN.com | Format-List AutoExpandingArchiveEnabled           # Verify
+Start-ManagedFolderAssistant -Identity USER@DOMAIN.com -FullCrawl               # Force start auto archiving immediately, and not wait for scheduled start.
+
+# "-FullCrawl"
+# The full crawl option ensures that all items in the mailbox are thoroughly examined during this process, even if they have been previously processed. This can be useful in situations where you want to reevaluate and apply policies to all items, not just those that have been modified since the last crawl.
 
 
-## Complance Hold Tag
-# If archiving has not started and 24 hours has passed, there can be a 'Compliance Hold Tag' on the mailbox.  This can only be removed from Microsoft's end as of 2023/08/29.
-#
-## RetentionHoldEnabled
-# You can also check the mailbox to see if it has "RetentionHoldEnabled" to $true.  If it set to $true it will not permit retention policies to run.
-# Check Mailbox
+## Compliance Hold Tag
+# If archiving has not started and 24 hours has passed, there can be a 'Compliance Hold Tag' on the mailbox. You can check this by identifying if "RetentionHoldEnabled" is set to $true.
 Get-Mailbox USER@DOMAIN.com | Select-Object RetentionHoldEnabled
-# Remove Retention Hold
+# Disable Retention Hold & Enable Retention Policies
 Set-Mailbox USER@DOMAIN.com -RetentionHoldEnabled $false
