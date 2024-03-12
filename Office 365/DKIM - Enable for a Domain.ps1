a99a10cfb87d93b5bce4e6b2e214e5126f31a26f
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-03-12 11:33:31 -0400

    Update 2024-03-12

--- Office 365/DKIM - Enable for a Domain.ps1
+++
@@ -1,5 +0,0 @@
-# Update domain.com to the public domain's actual address
-# Work in Progress / NOT Finished
-
-Get-Command -Module tmp_dmwhjqj3.m1c *dkim*
-New-DkimSigningConfig -DomainName DOMAIN.com -Enabled $True
