242adc52f2f0fce43ee265789197027630bdb9d5
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-03-21 11:25:45 -0400

    Update 2024-03-21

---
+++ PHP/PHP_V8.3.4.php
@@ -0,0 +1,9 @@
+/* PHP_v8.3.4.zip downloaded from https://windows.php.net/download#php-8.3
+Installation:
+1. Extract to C:/PHP (Example - C:/PHP/php.exe)
+2. Start > Run 'sysdm.cpl'
+3. Advanced tab > Select 'Path' > Select 'Edit' > Select 'New' > Add 'C:\PHP' > Select 'Ok', Select 'Ok' again, and Select 'Ok' a final time to close System Properties.
+*/
+
+// Test PHP Installation
+PHP -v
