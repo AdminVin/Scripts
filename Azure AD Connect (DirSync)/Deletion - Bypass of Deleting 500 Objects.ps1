0371e41af219da8e0bdeb0767403129f2ad5810e
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-07-30 16:00:01 -0400

    Update 2024/07/30

--- Azure AD Connect (DirSync)/Deletion - Bypass of Deleting 500 Objects.ps1
+++ Azure AD Connect (DirSync)/Deletion - Bypass of Deleting 500 Objects.ps1
@@ -1,11 +1,12 @@
 #region Notes
-<# This needs to be run on the server/computer that has Azure AD Connect installed on it #>
+<# This needs to be run on the server/computer that has Azure AD Connect installed on it 
+Import-Module ADSync also must be ran first or commands will fail
+#>
 #endregion
-
 
 # Disable Delete Protection
 Disable-ADSyncExportDeletionThreshold
 
 # Enable Delete Protection
-Enable-ADSyncExportDeletionThreshold
-# Enter "500" as that is the default object limit
+Enable-ADSyncExportDeletionThreshold -DeletionThreshold 500
+# 500 is the default object limit
