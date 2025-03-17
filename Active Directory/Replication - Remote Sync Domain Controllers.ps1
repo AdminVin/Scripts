439e6428c79962c3e656263a4f47f38dc125f338
Vincent <1580359+AdminVin@users.noreply.github.com>
2025-03-17 15:25:37 -0400

    Update 2025-03-17

--- Active Directory/Replication - Remote Sync Domain Controllers.ps1
+++ Active Directory/Replication - Remote Sync Domain Controllers.ps1
@@ -1,9 +1,18 @@
-repadmin /syncall /AedP
-
 <#
-Arugments
+Arguments
 /A: Initiates asynchronous replication.
-/e: Synchronizes enterprise partitions.
+    - Replicates all partitions.
+/e: Synchronizes enterprise partitions (applies to a single forest with multiple child domains).
+    - Examples:
+     ✅ domain.local
+     ✅ child.domain.local
+     ✅ sub.child.domain.local
 /d: Synchronizes the domain partition.
-/P: Skips the initial check for updates before synchronization.
+    - Includes all sites in Active Directory Sites and Services
+/p: Forces a PULL replication
+    - From the local DC to its replication partners
+/P: Forces a PUSH replication
+    - From the local DC to its replication partners
 #>
+
+repadmin /syncall /AdP
