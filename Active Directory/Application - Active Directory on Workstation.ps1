4e7ddb4acfbbe30dcf9e15c7ec50e41caa716b07
Vincent <1580359+AdminVin@users.noreply.github.com>
2025-08-27 13:12:43 -0400

    Update 2025-08-27

--- Active Directory/Application - Active Directory on Workstation.ps1
+++ Active Directory/Application - Active Directory on Workstation.ps1
@@ -1,11 +1,14 @@
+#> WSUS Disable (Temporarily)
 # Disable WSUS (if applicable)
 $currentWU = Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"-Name "UseWUServer"| Select-Object -ExpandProperty UseWUServer
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"-Name "UseWUServer"-Value 0
 Restart-Service wuauserv
 
-# Install Active Directory
-Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
-
 # Enable WSUS (if applicable)
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"-Name "UseWUServer"-Value $currentWU
 Restart-Service wuauserv
+
+
+
+#> Active Directory (Core / Server)
+Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
