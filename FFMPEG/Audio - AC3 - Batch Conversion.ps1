45919882c7688057ef2944de7ffecef5ab050c03
VINCENT BRIFFA <1580359+AdminVin@users.noreply.github.com>
2026-02-27 11:36:18 -0500

    Update 2026-02-27

--- FFMPEG/Audio - AC3 - Batch Conversion.ps1
+++ FFMPEG/Audio - AC3 - Batch Conversion.ps1
@@ -2,6 +2,10 @@
 # Add FFMPEG to the PATH (sysdm.cpl > Advanced > Enviroment Variables > Select Path > Edit > New > Add FFMPEG 'bin' Location)
 # Alternatively: Copy ffmpeg.exe, ffplay.exe, and ffprobe.exe to C:/Windows/System32
 
+# Quality Limits:
+# Movies: 640k 6 Channels / 320k 2 Channels
+# TV Shows: 320k 6 Channels / 256k 2 Channels
+
 # --- CONFIG ---
 $TV_DIR = "\\192.168.103.40\Media\TV\Bob's Burgers (2011)"
 
@@ -23,10 +27,10 @@ foreach ($file in $videoFiles) {
 
     # --- STEP 2: Convert using FFMPEG ---
     try {
-        # Stereo (2.0 / 320K)
-        & ffmpeg "-i" "$origFile" "-c:v" "copy" "-c:a" "ac3" "-b:a" "320k" "-ac" "2" "-af" "volume=0.3dB" "$convertedFile"
+        # Stereo (2.0 / 256K)
+        & ffmpeg "-i" "$origFile" "-c:v" "copy" "-c:a" "ac3" "-b:a" "256k" "-ac" "2" "-af" "volume=0.3dB" "$convertedFile" "-sn"
         # Surround (5.1 / 640k [Max])
-        #& ffmpeg "-i" "$origFile" "-c:v" "copy" "-c:a" "ac3" "-b:a" "320k" "-ac" "6" "-af" "volume=0.3dB" "$convertedFile"
+        #& ffmpeg "-i" "$origFile" "-c:v" "copy" "-c:a" "ac3" "-b:a" "320k" "-ac" "6" "-af" "volume=0.3dB" "$convertedFile" "-sn"
     } catch {
         Write-Host "Error converting:" $origFile -ForegroundColor Red
         Write-Host $_
